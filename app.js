6392f562f5f4dee528115f8c4677462b62e91752
anti <57675021+xnti@users.noreply.github.com>
2021-03-30 02:54:06 +0300

    Discord Nitro is 24 char randomized.

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ client.on('message', message => {
         let code = "";
         let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         
-        for (var i = 0; i < 18; i++) {
+        for (var i = 0; i < 24; i++) {
             code = code + letters.charAt(Math.floor(Math.random() * letters.length));
         }
         return code;
